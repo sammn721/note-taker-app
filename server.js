7243fd08d6a3ca252e25d7bdf75be7d19f41c07d
Sam Nielsen <sammn721@gmail.com>
2021-07-28 16:02:50 -0700

    things aren't working. modularizing.

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 // const util = require('util');
 const express = require('express');
 const path = require('path');
+const api = require('routes/index.js');
 const { v4: uuidv4 } = require('uuid');
 const { 
-    readAndAppend,
     readFromFile,
-    writeToFile
+    writeToFile,
+    readAndAppend
 } = require('./helpers/fsUtils.js');
 
 const app = express();
@@ -15,38 +16,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('root')
-// });
 
-// const readFromFile = util.promisify(fs.readFile);
-// /**
-//  * @param {string} destination
-//  * @param {object} content
-//  * @returns {void}
-//  */
-// const writeToFile = (destination, content) =>
-// fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-// err ? console.error(err) : console.info(`\nData written to ${destination}`)
-// );
-// /**
-//  * @param {object} content
-//  * @param {string} file
-//  * @returns {void}
-//  */
-// const readAndAppend = (content, file) => {
-//     fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-//         const parsedData = JSON.parse(data);
-//         parsedData.push(content);
-//         writeToFile(file, parsedData);
-//     }
-//     });
-// };
 app.get('/notes', (req, res) =>
-    // return notes.html
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
@@ -73,12 +44,6 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.json('Error in posting new note');
     }
-    // const newNote = req.body;
-    // fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    //     const notes = JSON.parse(data);
-    //     const notesJSON = JSON.stringify(notes);
-    // });
-    // res.send('posted');
 });
 
 app.get('*', (req, res) =>
