67690760e8310b3a74038e8faf529b6ddf655752
Sam Nielsen <sammn721@gmail.com>
2021-07-28 00:29:27 -0700

    post path complete?

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('root')
 });
@@ -19,8 +22,14 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    res.send('posted')
-})
+    console.log(req.body);
+    const newNote = req.body;
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+        const notes = JSON.parse(data);
+        const notesJSON = JSON.stringify(notes);
+    });
+    res.send('posted');
+});
 
 app.get('*', (req, res) => {
     res.send('all')
@@ -30,38 +39,13 @@ app.listen(PORT, () => {
     console.log(`App listening on PORT http://localhost:${PORT}`);
 });
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
 // const newNote = {
 //     "title":"A new note",
 //     "text":"Testing text"
 // };
 
-// // fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-// //     const notes = JSON.parse(data);
-// //     const notesJSON = JSON.stringify(notes);
-// // });
- 
-// app.get('/', function (req, res) {
-//   //return index.html
-// });
 
-// app.get('/notes', function (req, res) {
-//     //return notes.html
-// });
 
-// app.get('/api/notes', function (req, res) {
-//     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-//         const notes = JSON.parse(data);
-//         const notesJSON = JSON.stringify(notes);
-//     });
-// });
-
-// app.post('/api/notes', function (req, res)  {
-//     //receive new note to save on request body
-//     //add note to db.json files
-    
-//     //return new note
-//     //asign id with uniqid
+// app.delete('/api/notes/:id', (req, res) => {
+//     res.send('deleted')
 // });
